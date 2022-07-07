6b461d61db4073aed21ab43a05b58334f05f5462
JohannesNicholas <45587025+Goanna7007@users.noreply.github.com>
2022-07-07 12:18:19 +1000

    How working

--- bot.py
+++ bot.py
@@ -1,22 +1,36 @@
-import discord
+import discord # pycord
 import secrets
 
 bot = discord.Bot()
 
+#when the bot is connected
 @bot.event
 async def on_ready():
-    print(f"We have logged in as {bot.user}")
+    await log(f"We have logged in as {bot.user}")
 
-@bot.slash_command(guild_ids=[994167372090966016])
+
+#slash commands
+@bot.slash_command()
 async def hello(ctx):
     await ctx.respond("Hello!")
 
-@bot.slash_command(guild_ids=[994167372090966016])
+@bot.slash_command()
 async def poll(ctx):
     await ctx.respond("Hello!")
 
-@bot.slash_command(guild_ids=[994167372090966016])
+@bot.slash_command()
 async def about(ctx):
-    await ctx.respond("Hello!")
+    await ctx.respond("""Hi, I am a bot written in python by Johannes (Joey) Nicholas <@282428409685213184>. 
+Joey takes all responsibility for my actions. 
+Contact: johannes.nicholas@utas.edu.au. 
+View my source code: https://github.com/JohannesNicholas/RoboJoey""")
 
+
+#used to log everything
+async def log(message):
+    print("Log: " + message)
+    await bot.get_channel(994423152924966932).send(message)
+
+
+#run the bot
 bot.run(secrets.botToken)
